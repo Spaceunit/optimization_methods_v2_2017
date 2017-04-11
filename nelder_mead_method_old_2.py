ada970770e140fbc4965e42e0abc90ff8c5c1f2b
oleksiy <spaceunit>
2017-04-11 19:08:21 +0300

    changes in Nelder-Mead method

--- nelder_mead_method_old_2.py
+++ nelder_mead_method_old_2.py
@@ -90,12 +90,15 @@ class NMM:
     def makedefault(self):
         self.epsilon[0] = 10 ** (-self.accuracy)
         self.epsilon[1] = self.epsilon[0]
-        #self.expression = expression.Expression("Function", "4*(x1-2)**2+(x2-1)**2")
-        self.expression = expression.Expression("Function", "4*(x1-5)**2+(x2-6)**2")
+        self.expression = expression.Expression("Function", "4*(x1-2)**2+(x2-1)**2")
         self.expression.parameters["unimodal"] = True
-        self.expression.parameters["global_min"] = [5.0, 6.0]
-        self.x_start = [[8.0, 9.0], [10.0, 11.0], [8.0, 11.0]]
-
+        self.x_start = [[5.0, 4.0], [7.0, 6.0], [5.0, 6.0]]
+        self.x_delta = [[5.0, 4.0], [7.0, 6.0], [5.0, 6.0]]
+        self.result = {"i": [], "xk": [], "fx": [], "action": []}
+        self.cross = []
+        self.h = self.epsilon
+        self.msycle = 3
+        self.mm = True
 
     def importparam(self, accuracy):
         self.accuracy = accuracy
@@ -160,12 +163,73 @@ class NMM:
 
     def resolve(self):
         self.makedefault()
+        k = 0
+        sycle = [0,0,0]
+        x_mod = self.deepcopy(self.x_start)
+        while k <= 30:
+            f = []
+            x = NMM.deepcopy(x_mod)
+            for i in range(len(x)):
+                f.append(self.expression.execute_l(x[i]))
 
-        pass
+            self.collect_data(k, x, f, "initial simplex")
+
+            xmax = x[f.index(max(f))].copy()
+            xmin = x[f.index(min(f))].copy()
+            xmed= [0.0, 0.0]
+            for item in x:
+                index = x.index(item)
+                if index != x.index(xmax) and index != x.index(xmin):
+                    xmed = item.copy()
+            xhalf = self.mul(self.sum(xmin, xmed), 0.5)
+
+            xtest = self.sum(self.mul(self.dif(xhalf, xmax), 2.0), xmax)
+            f_xtest = self.expression.execute_l(xtest)
+            if f_xtest < f[x.index(xmed)] and f_xtest > f[x.index(xmin)]:
+                xnew = self.sum(self.mul(self.dif(xhalf, xmax), 2.0), xmax)
+            elif (f_xtest < max(f) and f_xtest > f[x.index(xmed)]):
+                xnew = self.sum(self.mul(self.dif(xhalf, xmax), 0.5), xmax)
+            else:
+                xnew = self.sum(self.mul(self.dif(xhalf, xmax), 0.5), xmax)
+
+            x_mod = [xnew.copy(), xmed.copy(), xmin.copy()]
+
+            if self.compare(x, x_mod):
+                for item in x_mod:
+                    if item in x:
+                        sycle[x_mod.index(item)] += 1
+
+                for item in sycle:
+                    if item >= self.msycle:
+                        center = item.copy()
+                        x_ch = []
+                        for item1 in x_mod:
+                            index = x.index(item1)
+                            if index != x_mod.index(center):
+                                x_ch.append(item1.copy())
+
+                        arr = self.reduction(center, x_ch[0], x_ch[1])
+                        x_mod = [center, x_ch[0], x_ch[1]]
+                        f = []
+                        for i in range(len(x_mod)):
+                            f.append(self.expression.execute_l(x_mod[i]))
+                        self.collect_data(k, x_mod, f, "do reduction")
+                        sycle = [0, 0, 0]
+                        break
+                    else:
+                        self.collect_data(k, x_mod, f, "make new simplex")
+            else:
+                self.collect_data(k, x_mod, f, "make new simplex")
+            #x = self.deepcopy(x_mod)
+            k += 1
+            print(k)
+        self.printresult()
 
     @staticmethod
     def reduction(center, x1, x2):
-        pass
+        half_x1 = NMM.mul(NMM.sum(x1, center), 0.5)
+        half_x2 = NMM.mul(NMM.sum(x2, center), 0.5)
+        return [half_x1, half_x2]
 
     @staticmethod
     def compare(x1, x2):
@@ -183,6 +247,31 @@ class NMM:
             r = False
             print("Halting check! - True")
         return r
+
+    @staticmethod
+    def find_finest_simplex(ex, x, dx, mm):
+        result = False
+        simplex = [
+
+        ]
+
+        f = []
+        for i in range(len(simplex)):
+            f.append(ex.execute_l(simplex[i]))
+        if mm:
+            result = min(f)
+            if not result < ex.execute_l(x):
+                result = False
+            else:
+                result = simplex[f.index(result)]
+        else:
+            result = max(f)
+            if not result > ex.execute_l(x):
+                result = False
+            else:
+                result = simplex[f.index(result)]
+
+        return result
 
     def get_alpha(self):
         return 0.5
@@ -235,11 +324,84 @@ class NMM:
         self.result["fx"].append(fx)
         self.result["action"].append(action)
 
+    def printresult_g0(self):
+        verts = []
+        for i in range(len(self.result["xk"])):
+            if not ("decrease x-delta" in self.result["action"][i]):
+                verts.append((self.result["xk"][i][0], self.result["xk"][i][1]))
+        print("Points count:", len(verts))
+        path = Path(verts)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        patch = patches.PathPatch(path, facecolor='none', lw=2)
+        ax.add_patch(patch)
+
+        xs, ys = zip(*verts)
+        ax.plot(xs, ys, 'x--', lw=2, color='black', ms=10)
+
+        plt.show()
+
     def printresult_g(self):
-        pass
+        verts = []
+        for i in range(len(self.result["xk"])):
+        #    if not ("decrease x-delta" in self.result["action"][i]):
+            verts.append((self.result["xk"][i][0], self.result["xk"][i][1]))
+        print("Points count:", len(verts))
+        path = Path(verts)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        patch = patches.PathPatch(path, facecolor='none', lw=2)
+        ax.add_patch(patch)
+
+        xs, ys = zip(*verts)
+        ax.plot(xs, ys, 'x--', lw=2, color='black', ms=10)
+
+        plt.show()
 
     def printresult_3d(self):
-        pass
+        verts = [[], [], []]
+        for i in range(len(self.result["xk"])):
+            if not ("decrease x-delta" in self.result["action"][i]):
+                verts[0].append(self.result["xk"][i][0])
+                verts[1].append(self.result["xk"][i][1])
+                verts[2].append(self.result["fx"][i])
+        print("Points count:", len(verts[0]))
+
+
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+
+        # Make data.
+        X = np.arange(-5, 5, 0.25)
+
+        Y = np.arange(-5, 5, 0.25)
+
+        X, Y = np.meshgrid(X, Y)
+        #R = np.sqrt(X ** 2 + Y ** 2)
+        Z = np.array([self.expression.execute_l([X[i], Y[i]]) for i in range(len(X))])
+
+        # Plot the surface.
+        surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                               linewidth=0, antialiased=False)
+
+        #X = np.array(verts[0])
+        #Y = np.array(verts[1])
+        #Z = np.array([self.expression.execute_l([verts[0][i], verts[1][i]]) for i in range(len(verts[2]))])
+
+        #surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+        #                       linewidth=0, antialiased=False)
+
+        # Customize the z axis.
+        ax.set_zlim(-1.01, 1.01)
+        ax.zaxis.set_major_locator(LinearLocator(10))
+        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+        # Add a color bar which maps values to colors.
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+
+        plt.show()
 
     def printresult(self):
         print('')
